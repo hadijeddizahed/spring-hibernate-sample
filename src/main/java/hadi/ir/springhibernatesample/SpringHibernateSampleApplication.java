ce03a47e7735723943e739c427862a2566c19fe6
Hadi <hadijeddizahed@gmail.com>
2018-06-14 20:34:21 +0430

    Initial sample project
    create maven base project with spring boot

---
+++ src/main/java/hadi/ir/springhibernatesample/SpringHibernateSampleApplication.java
@@ -0,0 +1,12 @@
+package hadi.ir.springhibernatesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/hadi/ir/springhibernatesample/SpringHibernateSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package hadi.ir.springhibernatesample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHibernateSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
